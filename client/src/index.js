85a55bf2e23a90c8062616852eb721881f76e457
Nikhil Das <godabanikhildas98@gmail.com>
2025-11-06 01:24:35 +0530

    feat: integrate Redux Toolkit for state management and update dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import reducers from "./reducers";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = configureStore({
+  reducer: reducers,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: process.env.NODE_ENV !== "production",
+});
 root.render(
   <Provider store={store}>
     <React.StrictMode>
